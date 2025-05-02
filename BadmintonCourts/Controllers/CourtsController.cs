2a9c7aea891cec623ec3ab4e0b04da718663f46c
ac121446 <ac121446@8CG8163KZV>
2025-05-02 13:59:48 +1200

    removed use another program to login/ register

--- BadmintonCourts/Controllers/CourtsController.cs
+++ BadmintonCourts/Controllers/CourtsController.cs
@@ -27,7 +27,7 @@ namespace BadmintonCourts.Controllers
         }
 
         // GET: Courts/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(int? id)       
         {
             if (id == null)
             {
